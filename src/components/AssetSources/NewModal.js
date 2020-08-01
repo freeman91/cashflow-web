8cbedea978438cba0e5f8bf1a9f2fdd73a5953c0
freeman91 <addisonfreeman91@gmail.com>
2020-08-01 13:46:53 -0400

    Init asset source front end components

---
+++ src/components/AssetSources/NewModal.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+
+// reactstrap components
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  Modal,
+  ModalBody,
+  ModalHeader,
+} from 'reactstrap';
+
+import AssetSource from '../../service/AssetSourceService';
+
+const defaultState = {
+  open: false,
+  value: {
+    name: '',
+    description: '',
+  },
+};
+
+class NewModal extends Component {
+  state = { ...defaultState };
+
+  handleChange = (event) => {
+    this.setState({
+      value: {
+        ...this.state.value,
+        [event.target.id]: event.target.value,
+      },
+    });
+  };
+
+  handleSubmit = () => {
+    const { value } = this.state;
+    const { user, getData, handleClose } = this.props;
+    if (value.name === '') {
+      console.error('[ERROR]: Invalid data in input field');
+    } else {
+      AssetSource.create(
+        {
+          name: value.name,
+          description: value.description,
+        },
+        user.auth_token
+      ).then((result) => {
+        if (result.status === 201) {
+          this.setState({ ...defaultState });
+          getData();
+          handleClose();
+        }
+      });
+    }
+  };
+
+  render() {
+    const { open, handleClose } = this.props;
+    return (
+      <Modal isOpen={open} toggle={handleClose} modalClassName="modal-info">
+        <ModalHeader>New Asset Source</ModalHeader>
+        <ModalBody>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend"> </InputGroupAddon>
+            <Input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="name"
+              onChange={this.handleChange}
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend"> </InputGroupAddon>
+            <Input
+              type="text"
+              name="description"
+              id="description"
+              placeholder="description"
+              onChange={this.handleChange}
+            />
+          </InputGroup>
+          <InputGroup>
+            <Button onClick={handleClose} color="default">
+              Cancel
+            </Button>
+            <Button onClick={this.handleSubmit} color="primary">
+              Submit
+            </Button>
+          </InputGroup>
+        </ModalBody>
+      </Modal>
+    );
+  }
+}
+
+export default NewModal;
---
+++ src/service/AssetSourceService.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import API_HOST from '../helpers/api-host.js';
+
+const create = async function (newSources, auth_token) {
+  return axios.post(API_HOST + '/property_sources', {
+    headers: { Authorization: auth_token },
+    params: {
+      name: newSources.name,
+      description: newSources.description,
+    },
+  });
+};
+
+const destroy = async function (id, auth_token) {
+  return axios.delete(API_HOST + '/property_sources', {
+    headers: { Authorization: auth_token },
+    params: {
+      id: id,
+    },
+  });
+};
+
+const getSources = async function (auth_token) {
+  return axios
+    .get(API_HOST + '/property_sources/all', {
+      headers: { Authorization: auth_token },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.error(error));
+};
+
+const update = async function (assetSource, auth_token) {
+  return axios
+    .post(API_HOST + '/property_sources/update', {
+      headers: { Authorization: auth_token },
+      params: {
+        id: assetSource.id,
+        name: assetSource.name,
+        description: assetSource.description,
+      },
+    })
+    .catch((error) => console.log(error));
+};
+
+export default {
+  create,
+  destroy,
+  getSources,
+  update,
+};
