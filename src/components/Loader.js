fe2c9ec57ed91c8c218e140c9c1fc8a71ef4bb7b
freeman91 <addisonfreeman91@gmail.com>
2020-08-02 11:46:26 -0400

    Remove material-ui package

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,39 +1,22 @@
-import React, { Component } from "react";
-import { Grid, withStyles } from "@material-ui/core";
-import LoaderClass from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import React, { Component } from 'react';
+import LoaderClass from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-const styles = (theme) => ({
-  loader: {
-    height: "100%",
-    position: "relative",
-  },
-});
+const loader = {
+  position: 'relative',
+  marginTop: '45vh',
+  top: '50%',
+  left: '50%',
+};
 
 class Loader extends Component {
   render() {
-    const { classes } = this.props;
-
     return (
-      <div className={classes.loader}>
-        <Grid
-          container
-          style={{ minHeight: "100vh" }}
-          spacing={0}
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <LoaderClass
-            type="TailSpin"
-            color="#00BFFF"
-            height={100}
-            width={100}
-          />
-        </Grid>
+      <div style={loader}>
+        <LoaderClass type="TailSpin" color="#00BFFF" height={100} width={100} />
       </div>
     );
   }
 }
 
-export default withStyles(styles)(Loader);
+export default Loader;
