5814b59c9c9a75cdc6340bfe65e919246ce473f9
freeman91 <addisonfreeman91@gmail.com>
2020-08-15 11:11:55 -0400

    pass date params

--- src/service/NetWorthService.js
+++ src/service/NetWorthService.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import API_HOST from '../helpers/api-host.js';
 
-const getData = async function (auth_token) {
+const getData = async function (auth_token, week, year) {
   return axios
     .get(API_HOST + '/networth/data', {
       headers: { Authorization: auth_token },
+      params: {
+        week,
+        year,
+      },
     })
     .then((response) => {
       return response.data;
--- src/service/YearService.js
+++ src/service/YearService.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import API_HOST from '../helpers/api-host.js';
 
-const getData = async function (auth_token) {
+const getData = async function (auth_token, year) {
   return axios
     .get(API_HOST + '/year/data', {
       headers: { Authorization: auth_token },
+      params: {
+        year,
+      },
     })
     .then((response) => {
       return response.data;
