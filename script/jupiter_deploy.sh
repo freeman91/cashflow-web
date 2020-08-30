6ed3485abf15497bc327c8dbe79e187e24ea5353
freeman91 <addisonfreeman91@gmail.com>
2020-08-30 10:13:22 -0400

    Kill servers gracefully

--- script/jupiter_deploy.sh
+++ script/jupiter_deploy.sh
@@ -6,17 +6,8 @@ red="\033[31m"
 yellow="\033[33m"
 green="\033[32m"
 endColor="\033[97m"
-check="✔"
-cross="✘"
 
-echo -e "${yellow}Shutting down cashflow servers${endColor}"
-# kill the front end server
-servePID=$(ps -c | grep node | xargs)
-servePID=$(echo $servePID | egrep -o "^[0-9]*\s")
-kill -9 $(echo $servePID)
-
-# kill the backend server
-kill -9 $(cat tmp/pids/server.pid)
+zsh script/kill_servers.sh
 
 echo -e "${yellow}Fetching and building latest cashflow repo${endColor}"
 # update local repo from origin master
