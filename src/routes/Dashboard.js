3332e1f93a184e4a32cf8e9e9fa0088c1d96b105
freeman91 <addisonfreeman91@gmail.com>
2020-09-02 13:04:43 -0400

    Update line chart card with stats

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -15,6 +15,8 @@ import {
   CardHeader,
   CardTitle,
   Col,
+  Container,
+  Table,
   Row,
 } from 'reactstrap';
 
@@ -29,12 +31,13 @@ import IncomeNewModal from '../components/Income/NewModal';
 import WorkHourNewModal from '../components/WorkHour/NewModal';
 import formatDateObject from '../helpers/format-date-object';
 import formatter from '../helpers/currency';
+import { renderDateMonthName } from '../helpers/render-date';
 
 const defaultState = {
   isLoaded: false,
   expenses: [],
-  incomes: [],
-  workHours: [],
+  incomeTotal: 0,
+  workHourTotal: 0,
   dialogs: {
     expOpen: false,
     incOpen: false,
@@ -55,8 +58,9 @@ const getMonday = (d) => {
 };
 
 const prepareChartData = (expenses) => {
+  let expenseTotal = 0;
   const today = new Date();
-  let ret = [];
+  let chartData = [];
   for (let i = 0; i < expenses.length; i++) {
     if (expenses[i].length === 0) break;
     let currentDate = getMonday(expenses[i][expenses[i].length - 1][1]);
@@ -67,15 +71,16 @@ const prepareChartData = (expenses) => {
           expenseSum += expense[0];
         }
       });
-      ret.push({
+      chartData.push({
         name: formatDateObject(currentDate),
         expense: Math.round(expenseSum * 100) / 100,
       });
+      expenseTotal += expenseSum;
       currentDate.setDate(currentDate.getDate() + 1);
       if (currentDate > today) j = 10;
     }
   }
-  return ret;
+  return { chartData, expenseTotal };
 };
 
 class Dashboard extends Component {
@@ -92,11 +97,9 @@ class Dashboard extends Component {
   getDashData = async (type) => {
     DashboardService.getData(this.props.user.auth_token).then((result) => {
       this.setState({
-        netIncomeYear: result.net_income_year,
-        NetIncomeMonth: result.net_income_month,
         expenses: result.expenses,
-        incomes: result.incomes,
-        workHours: result.workHours,
+        incomeTotal: result.incomeTotal,
+        workHourTotal: result.workHourTotal,
         isLoaded: true,
         reload: {
           ...defaultState.reload,
@@ -136,123 +139,173 @@ class Dashboard extends Component {
     const { user } = this.props;
     const {
       isLoaded,
-      // netIncomeYear,
-      // netIncomeMonth,
       reload,
       expenses,
-      // incomes,
-      // workHours,
+      incomeTotal,
+      workHourTotal,
     } = this.state;
     if (!isLoaded) return <Loader />;
 
-    const chartData = prepareChartData(expenses);
+    const { chartData, expenseTotal } = prepareChartData(expenses);
     return (
       <>
         <div className="content">
-          <Row>
-            <Col xs="2">
-              <Card>
-                <CardHeader>
-                  <CardTitle className="card-title" tag="h3">
-                    Enter Data
-                  </CardTitle>
-                </CardHeader>
-                <CardBody className="card-body">
-                  <Row>
-                    <Button
-                      block
-                      size="sm"
-                      color="primary"
-                      onClick={this.handleCreateClickExpense}
-                    >
-                      New Expense
-                    </Button>
-                  </Row>
-                  <Row>
-                    <Button
-                      block
-                      size="sm"
-                      color="primary"
-                      onClick={this.handleCreateClickIncome}
-                    >
-                      New Income
-                    </Button>
-                  </Row>
-                  <Row>
-                    <Button
-                      block
-                      size="sm"
-                      color="primary"
-                      onClick={this.handleCreateClickWorkHour}
-                    >
-                      New Work Hour
-                    </Button>
-                  </Row>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col xs="10">
-              <Card>
-                <CardBody
-                  style={{
-                    flex: '1 1 auto',
-                    overflowY: 'auto',
-                    minHeight: '500px',
-                    width: '100%',
-                  }}
-                >
-                  <ResponsiveContainer minHeight="250" minWidth="250">
-                    <BarChart
-                      height="500"
-                      width="700"
-                      data={chartData}
-                      margin={{ top: 15, right: 30, left: 20, bottom: 5 }}
-                    >
-                      <XAxis
-                        dataKey="name"
-                        tickFormatter={(date) => {
-                          return date.slice(5, 10).replace('-', '/');
-                        }}
-                      />
-                      <YAxis />
-                      <Tooltip
-                        formatter={(value) => {
-                          return formatter.format(value);
-                        }}
-                      />
-                      <Bar dataKey="expense" fill="#8884d8" />
-                    </BarChart>
-                  </ResponsiveContainer>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="5">
-              <ExpenseTable
-                user={user}
-                reload={reload.expense}
-                getData={this.getDashData}
-                stopReload={this.stopReload}
-              />
-            </Col>
-            <Col xs="4">
-              <IncomeTable
-                user={user}
-                reload={reload.income}
-                getData={this.getDashData}
-                stopReload={this.stopReload}
-              />
-            </Col>
-            <Col xs="3">
-              <WorkHourTable
-                user={user}
-                reload={reload.workHour}
-                getData={this.getDashData}
-                stopReload={this.stopReload}
-              />
-            </Col>
-          </Row>
+          <Container>
+            <Row>
+              <Col xs="2">
+                <Card>
+                  <CardHeader>
+                    <CardTitle className="card-title" tag="h3">
+                      Enter Data
+                    </CardTitle>
+                  </CardHeader>
+                  <CardBody className="card-body">
+                    <Container>
+                      <Row>
+                        <Button
+                          block
+                          size="sm"
+                          color="primary"
+                          onClick={this.handleCreateClickExpense}
+                        >
+                          New Expense
+                        </Button>
+                      </Row>
+                      <Row>
+                        <Button
+                          block
+                          size="sm"
+                          color="primary"
+                          onClick={this.handleCreateClickIncome}
+                        >
+                          New Income
+                        </Button>
+                      </Row>
+                      <Row>
+                        <Button
+                          block
+                          size="sm"
+                          color="primary"
+                          onClick={this.handleCreateClickWorkHour}
+                        >
+                          New Work Hour
+                        </Button>
+                      </Row>
+                    </Container>
+                  </CardBody>
+                </Card>
+                <Card>
+                  <p />
+                </Card>
+              </Col>
+              <Col xs="10">
+                <Card>
+                  <CardBody>
+                    <Container>
+                      <Row>
+                        <Col xs="3">
+                          <CardTitle className="card-title" tag="h3">
+                            {`${renderDateMonthName(
+                              chartData[0].name
+                            )} - ${renderDateMonthName(
+                              chartData[chartData.length - 1].name
+                            )}`}
+                          </CardTitle>
+                          <Table style={{ marginTop: '4rem' }}>
+                            <tbody>
+                              <tr>
+                                <th>Expenses</th>
+                                <td>{formatter.format(expenseTotal)}</td>
+                              </tr>
+                              <tr>
+                                <th>Incomes</th>
+                                <td>{formatter.format(incomeTotal)}</td>
+                              </tr>
+                              <tr>
+                                <th>Net Income</th>
+                                <td>
+                                  {formatter.format(incomeTotal - expenseTotal)}
+                                </td>
+                              </tr>
+                              <tr>
+                                <th>Work Hours</th>
+                                <td>{workHourTotal}</td>
+                              </tr>
+                              <tr>
+                                <th>Hourly Wage</th>
+                                <td>
+                                  {formatter.format(
+                                    workHourTotal === 0
+                                      ? 0
+                                      : incomeTotal / workHourTotal
+                                  )}
+                                </td>
+                              </tr>
+                            </tbody>
+                          </Table>
+                        </Col>
+                        <Col xs="9" style={{ height: '400px' }}>
+                          <ResponsiveContainer minHeight="250" minWidth="250">
+                            <BarChart
+                              height="500"
+                              width="700"
+                              data={chartData}
+                              margin={{
+                                top: 15,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                              }}
+                            >
+                              <XAxis
+                                dataKey="name"
+                                tickFormatter={(date) => {
+                                  return date.slice(5, 10).replace('-', '/');
+                                }}
+                              />
+                              <YAxis />
+                              <Tooltip
+                                formatter={(value) => {
+                                  return formatter.format(value);
+                                }}
+                              />
+                              <Bar dataKey="expense" fill="#8884d8" />
+                            </BarChart>
+                          </ResponsiveContainer>
+                        </Col>
+                      </Row>
+                    </Container>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="5">
+                <ExpenseTable
+                  user={user}
+                  reload={reload.expense}
+                  getData={this.getDashData}
+                  stopReload={this.stopReload}
+                />
+              </Col>
+              <Col xs="4">
+                <IncomeTable
+                  user={user}
+                  reload={reload.income}
+                  getData={this.getDashData}
+                  stopReload={this.stopReload}
+                />
+              </Col>
+              <Col xs="3">
+                <WorkHourTable
+                  user={user}
+                  reload={reload.workHour}
+                  getData={this.getDashData}
+                  stopReload={this.stopReload}
+                />
+              </Col>
+            </Row>
+          </Container>
         </div>
         <ExpenseNewModal
           user={user}
